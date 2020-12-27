e88f1db88029b9f9cc2a7be9ff3bc3e0735d0bb9
DeFreitas-Samuel <52897285+DeFreitas-Samuel@users.noreply.github.com>
2020-12-27 00:02:48 -0400

    prueba de test_upper_and_lower_case_equality pasada en verde

--- isogram.py
+++ isogram.py
@@ -1,9 +1,21 @@
+def isIsogram(word):
+    charList = []
+    for char in word:
+        if char in charList:
+            return False
+        charList.append(char)
+    return True
 def isogramCounter(fileName):
     counter = 0
-    words = []
+    listOfWords = []
     isogramFile = open(fileName, "r")
     for lines in isogramFile:
-        counter += 1
+        cleanWord = lines.strip().lower()
+        listOfWords.append(cleanWord)
+    
+    for word in listOfWords:
+        if isIsogram(word) is True:
+            counter += 1
 
     isogramFile.close()
     return counter
--- isogram_test.py
+++ isogram_test.py
@@ -5,3 +5,5 @@ class testIsogram(unittest.TestCase):
     
     def test_blank_File(self):
         self.assertEqual(isogramCounter("blank.txt"), 0)
+    def test_upper_and_lower_case_equality(self):
+         self.assertEqual(isogramCounter("prueba1.txt"), 1)
