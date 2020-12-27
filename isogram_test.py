2a632bf9a77974e2418ef8f004aa80dd2c4a7c1d
DeFreitas-Samuel <52897285+DeFreitas-Samuel@users.noreply.github.com>
2020-12-26 23:47:15 -0400

    primer commit segundo dia

--- isogram_test.py
+++ isogram_test.py
@@ -2,11 +2,5 @@ import unittest
 from isogram import *
 
 class testIsogram(unittest.TestCase):
-    def testBlankFile(self):
-        self.assertEqual(isogramCounter("blank.txt"),0)
-    
-    def testUpperCaseAndLowerCaseAreEqual(self):
-        self.assertEqual(isogramCounter("Prueba1.txt"),1)
-    
-    def testIsogramList(self):
-        self.assertEqual(isogramCounter("Isogram list.txt"),4376)
+    def blankFile(self):
+        self.assertEqual(isogramCounter("blank.txt"), 0)
