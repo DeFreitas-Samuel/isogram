29535cd31975fd2e64cd1f8a85b784d741b6fd35
DeFreitas-Samuel <52897285+DeFreitas-Samuel@users.noreply.github.com>
2020-12-27 23:59:12 -0400

    prueba de las mayusculas y minusculas pasada en verde

--- isogram.py
+++ isogram.py
@@ -1,7 +1,19 @@
+def isIsogram(word):
+    newWord = word.strip().lower()
+    charlist = []
+    for char in newWord:
+        if char in charlist:
+            return False
+        charlist.append(char)
+    return True
+
 def isogramCounter(fileName):
     counter = 0
     isogramFile = open(fileName, "r")
     for lines in isogramFile:
-        counter +=1
-
+        if isIsogram(lines) is True:
+            counter += 1
+    isogramFile.close()
     return counter
+
+isIsogram("Asa")
--- isogram_test.py
+++ isogram_test.py
@@ -4,3 +4,6 @@ from isogram import *
 class testIsogram(unittest.TestCase):
     def test_blank_file(self):
         self.assertEqual(isogramCounter("blank.txt"), 0)
+
+    def test_equality_of_lower_and_upper_case(self):
+        self.assertEqual(isogramCounter("prueba1.txt"), 0)
