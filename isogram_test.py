cd9f392dcd7a124f426c7dae04215a388edca534
DeFreitas-Samuel <52897285+DeFreitas-Samuel@users.noreply.github.com>
2020-12-28 00:01:55 -0400

    la prueba isogram list paso en verde

--- isogram_test.py
+++ isogram_test.py
@@ -7,3 +7,6 @@ class testIsogram(unittest.TestCase):
 
     def test_equality_of_lower_and_upper_case(self):
         self.assertEqual(isogramCounter("prueba1.txt"), 0)
+
+    def test_isogram_list(self):
+        self.assertEqual(isogramCounter("isogram list.txt"), 4376)
